27196cafba4910d422e06436f6b7dac50c3735d6
CandiceBousquet <candice.bousquet@gmail.com>
2017-06-29 16:27:25 -0400

    Update AlarmForm.js

--- app/screens/AlarmForm.js
+++ app/screens/AlarmForm.js
@@ -113,7 +113,6 @@ export default class extends React.Component {
   }
 
 	render () {
-    console.log('waddddupppp', this.state);
 		return (
       <ScrollView style={styles.window}>
         <TextInput
