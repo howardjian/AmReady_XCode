a8c498d041720b142294706544a6bd029b44e6d4
Candice Bousquet <candice.bousquet@gmail.com>
2017-07-03 23:00:57 -0400

    add styling to route buttons

--- app/components/RouteOptions.js
+++ app/components/RouteOptions.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import {Text, View, Image } from 'react-native';
 import { Button } from 'native-base';
+import { Divider} from 'react-native-elements';
 
-export default function({transit,icon, index, selectRoute, duration}) {
+export default ({icon, duration}) => {
 
-  const d = Math.floor(duration / 60);
+ const d = Math.ceil(duration / 60);
 
-  return (
-    <View style={{paddingLeft: 15}}>
-
-
-      <Button bordered light>
-         <Text style={{color: 'white', fontSize: 18, paddingLeft: 15}}> <Image
-        style={{width: 20, height: 20}}
-         source = {{uri: `https:${icon}`}}
-      /> Duration: <Text style={{color: '#00BFFF', fontSize: 18, paddingLeft: 15}}> {' ' + d +' minutes'} </Text></Text>
-
-      </Button>
+ return (
+   <View >
 
 
+     <Button  style={{width: 340, alignSelf: 'center', backgroundColor: '#878787', borderRadius: 3}}>
+       <Text style={{color: '#00BFFF', fontSize: 18, paddingLeft: 15}}> <Image
+       style={{width: 20, height: 20}}
+        source = {{uri: `https:${icon}`}}
+     /> Duration: <Text style={{color: 'white', fontSize: 18, paddingLeft: 15}}> {' ' + d +' minutes'} </Text></Text>
+
+     </Button>
 
 
+     <Divider style={{paddingTop: 8, backgroundColor: '#333333'}} />
 
-    </View>
-  )
+
+   </View>
+ )
 }
