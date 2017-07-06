68045171c44daa6e21195103510e522149f1e974
Candice Bousquet <candice.bousquet@gmail.com>
2017-07-06 11:00:57 -0400

    adding real values to timer

--- app/features/Timer.js
+++ app/features/Timer.js
@@ -4,6 +4,8 @@ import { playAudio, stopAudio } from './Audio';
 import { getDuration } from '../../utils/utils';
 
 const SNOOZE_TIME_IN_MIN = 0.1;
+const SET_INTERVAL_TIME_IN_MIN = 15;
+const TIME_IN_MIN_BEFORE_CHECKING_DURATION = 120;
 
 let audioId = null;
 let localNotification = null;
@@ -16,11 +18,6 @@ export function resetAlarm(backgroundTimerId, setSnooze, alarm, alarmIndex) {
     stopAudio(audioId);
     if (setSnooze) setTimer(alarm, alarmIndex, null, true);
 }
-
-// export function clearBackgroundTimer(oldTimerId, newTimerId, type) {
-//     BackgroundTimer.clearTimeout(oldTimerId);
-//     if (newTimerId !== undefined) updateAlarmTimer(oldTimerId, newTimerId, type);
-// }
 
 export function clearBackgroundTimer(oldTimerId, newTimerId, updateAlarmTimer) {
     BackgroundTimer.clearTimeout(oldTimerId);
@@ -38,29 +35,22 @@ export function setTimer (alarm, alarmIndex, updateAlarmTimer, setSnooze = false
     } else {
         const arrivalTimeStr = alarm.arrivalTime;
         timeInMinUntilAlarmTriggers = calcTimeBeforeAlarmTriggers(arrivalTimeStr, +alarm.prepTime, +alarm.route.duration);
-        // console.warn(arrivalTimeStr, alarm.prepTime, alarm.duration);
+        let timeUntilWeStartCheckingDuration = timeInMinUntilAlarmTriggers - TIME_IN_MIN_BEFORE_CHECKING_DURATION;
+        timeUntilWeStartCheckingDuration = timeUntilWeStartCheckingDuration < 0 ? 0 : timeUntilWeStartCheckingDuration;
+        return setTimerToCheckDuration(timeUntilWeStartCheckingDuration, updateAlarmTimer, alarm, alarmIndex);
     }
-    // settimeout( setInterval(someNewFuncToFetchDuration, 15min ) ,2hoursBefore timeInMinUntilAlarmTriggers)
-    // someNewFuncToFetchDuration = fetchduration, run calcTimeBeforeAlarmTriggers, if difference, reset timeinMin
-
-    // console.warn('TIME', timeInMinUntilAlarmTriggers);
-
-    // console.warn('AFTER UPDATING', timeInMinUntilAlarmTriggers);
-
-    // NOTE: We modified the react-native-background-timer index.js file
-    // such that the output of the setTimeout function is invoked with the timerId.
-    // The timerId is passed to the notification object so the timer can be turned off eventually
-    // Normally, the timerId is returned from setTimeout, but we don't have access to it inside of the callback function
-
-    // return setTimerToCheckDuration(timeInMinUntilAlarmTriggers - (2*60*60000), updateAlarmTimer);
-    return setTimerToCheckDuration(5000, updateAlarmTimer, alarm, alarmIndex); // 10 sec
 }
 
-function setTimerToCheckDuration(time, updateAlarmTimer, alarm, alarmIndex) {
+// NOTE: We modified the react-native-background-timer index.js file
+// such that the output of the setTimeout function is invoked with the timerId.
+// The timerId is passed to the notification object so the timer can be turned off eventually
+// Normally, the timerId is returned from setTimeout, but we don't have access to it inside of the callback function
+
+function setTimerToCheckDuration(timeInMin, updateAlarmTimer, alarm, alarmIndex) {
     return BackgroundTimer.setTimeout(function () { // 1
         return (timerId) => {
         console.warn('1', timerId);
-        let timeInMinUntilAlarmTriggers = 4000;
+        // let timeInMinUntilAlarmTriggers = 4000;
           const setIntervalTimeoutId = BackgroundTimer.setInterval(function() {
                 return (timerId) => {
                     getDuration(alarm)
@@ -68,25 +58,24 @@ function setTimerToCheckDuration(time, updateAlarmTimer, alarm, alarmIndex) {
                         console.warn('2', timerId);
                         console.warn('pliss', duration);
                         // fetch duration
-                        // let timeInMinUntilAlarmTriggers = calcTimeBeforeAlarmTriggers(arrivalTimeStr, +alarm.prepTime, fetch());
-                        // let timeInMinUntilAlarmTriggers = 4000;
+                        let timeInMinUntilAlarmTriggers = calcTimeBeforeAlarmTriggers(alarm.arrivalTime, +alarm.prepTime, duration);
                         console.warn('BEFORE UPDATING', timeInMinUntilAlarmTriggers);
-                        if (timeInMinUntilAlarmTriggers <= 2000) { // UPDATE
+                        if (timeInMinUntilAlarmTriggers <= SET_INTERVAL_TIME_IN_MIN) { // UPDATE
                             // set ringer timeout
-                            let ringerTimeoutId = setAlarmTimer(0.1, alarm, alarmIndex); // UPDATE: timeInMinUntilAlarmTriggers
+                            let ringerTimeoutId = setAlarmTimer(timeInMinUntilAlarmTriggers, alarm, alarmIndex); // UPDATE: timeInMinUntilAlarmTriggers
                             console.warn('2.5', ringerTimeoutId);
                             // clear timeout and update the timer
                             clearBackgroundTimer(timerId, ringerTimeoutId, updateAlarmTimer);
                         }
-                        timeInMinUntilAlarmTriggers = timeInMinUntilAlarmTriggers - 2000;
+                        // timeInMinUntilAlarmTriggers = timeInMinUntilAlarmTriggers - 2000;
                     })
                 }
-          }, 2000) // UPDATE
+          }, SET_INTERVAL_TIME_IN_MIN * 60000)
           // clear timeout and update the timer
           console.warn('1.5', setIntervalTimeoutId);
-          clearBackgroundTimer(timerId, setIntervalTimeoutId, updateAlarmTimer); // 1
+          clearBackgroundTimer(timerId, setIntervalTimeoutId, updateAlarmTimer);
         }
-      }, time);
+      }, timeInMin * 60000);
 }
 
 function setAlarmTimer (timeInMin, alarm, alarmIndex) {
